5b4284c6704ac2e2b8dafbc5cc4c7f552f3b26d2
Akash <akashsanthosh0010@gmail.com>
2026-03-23 09:09:10 +0530

    cart-api-integration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
+const instance = axios.create({
   baseURL: "http://127.0.0.1:8000/api",
 });
 
-// attach token automatically
-api.interceptors.request.use((config) => {
+instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
 
   if (token) {
@@ -15,4 +14,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
+export default instance;
